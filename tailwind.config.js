70beb1e86e4c963a1667443bcc4dc7db357227f7
Anna Prusakova <hanna.prusakova@gmail.com>
2023-04-25 20:50:15 +0300

    init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      soft_red: 'hsl(10, 79%, 65%)',
+      cyan:  'hsl(186, 34%, 60%)',
+      dark_brown: 'hsl(25, 47%, 15%)',
+      medium_brown: 'hsl(28, 10%, 53%)',
+      cream: 'hsl(27, 66%, 92%)',
+      very_pale_orange: 'hsl(33, 100%, 98%)'
+    },
+    screens: {
+      'sm': '375px',
+      'lg': '1440px',
+    }
   },
   plugins: [],
 }
